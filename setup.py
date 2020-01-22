2d3e8c6912e5cbdabdacf66c6f30ee755677dd97
Laurent Lasudry <lasudry@gmail.com>
2020-01-22 14:13:16 +0100

    We need at least Products.GenericSetup 1.8.2
    This is because collective.privacy uses post_handler zcml attribute

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     install_requires=[
         # -*- Extra requirements: -*-
         'plone.api',
-        'Products.GenericSetup',
+        'Products.GenericSetup>=1.8.2',
         'setuptools',
         'z3c.jbot',
         'plone.directives.form',
